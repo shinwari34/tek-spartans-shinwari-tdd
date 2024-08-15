9d930bdade1f7fabc7913bbed8ae5b82b7ceb842
Himmat <Najeebullah.shinwari34@gmail.com>
2024-08-14 23:46:03 -0600

    Adding security test

--- src/test/java/tek/tdd/base/UIBaseClass.java
+++ src/test/java/tek/tdd/base/UIBaseClass.java
@@ -1,13 +1,14 @@
 package tek.tdd.base;
 
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import tek.tdd.page.HomePage;
 import tek.tdd.page.SignInPage;
+//import tek.tdd.utility.SeleniumUtility;
 import utility.SeleniumUtility;
 
-import java.util.logging.Logger;
 
 public class UIBaseClass extends SeleniumUtility {
 
