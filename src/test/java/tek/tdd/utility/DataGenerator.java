2a6750c2e14276239a37715c7bada6815a64752a
Himmat <Najeebullah.shinwari34@gmail.com>
2024-08-20 22:57:26 -0600

    Add Logs for Data Generator Class

--- src/test/java/tek/tdd/utility/DataGenerator.java
+++ src/test/java/tek/tdd/utility/DataGenerator.java
@@ -1,9 +1,17 @@
 package tek.tdd.utility;
 
+import org.apache.logging.log4j.LogManager;
+
+import java.util.logging.Logger;
+
 public class DataGenerator {
 
-    public static String genereteRandomEmail(String prefix) {
+    private static final Logger LOGGER = LogManager.getLogger(DataGenerator.class);
+
+    public static String generateRandomEmail(String prefix) {
         int random = (int) (Math.random() * 100000);
-        return prefix + random + "@gmail.com";
+        String email = prefix + random + "@gmail.com";
+        LOGGER.debug("Generated random email {}", email);
+        return email;
     }
 }
